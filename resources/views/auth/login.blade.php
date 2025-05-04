51ab688e1888addc5482913eda23aec145db782a
infomotin <motin.mmk.jr@gmail.com>
2025-05-04 16:37:50 +0600

    fronend theme segment with login user Controller login fixt with register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,6 +46,61 @@
     </form>
 </x-guest-layout> --}}
 
+{{-- <x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> --}}
+
+
+
 @extends('frontend.frontend_dashboard')
 @section('content')
 <!--Page Title-->
@@ -109,26 +164,32 @@
                         </div>
                         <div class="tab" id="tab-2">
                             <div class="inner-box">
-                                <h4>Sign in</h4>
-                                <form action="signin.html" method="post" class="default-form">
+                                <h4>Register</h4>
+                                <form method="POST" action="{{ route('register') }}" class="default-form">
+                                    @csrf
                                     <div class="form-group">
                                         <label>User name</label>
-                                        <input type="text" name="name" required="">
+                                        <input type="text" name="name" id="name" required="">
                                     </div>
                                     <div class="form-group">
                                         <label>Email address</label>
-                                        <input type="email" name="email" required="">
+                                        <input type="email" name="email" id="email" required="">
                                     </div>
                                     <div class="form-group">
                                         <label>Password</label>
-                                        <input type="password" name="name" required="">
+                                        <input type="password" name="password" id="password" required="">
                                     </div>
+                                    <div class="form-group">
+                                        <label>Password Confirmation</label>
+                                        <input type="password" name="password_confirmation" id="password_confirmation" required="">
+                                    </div>
+
                                     <div class="form-group message-btn">
                                         <button type="submit" class="theme-btn btn-one">Sign in</button>
                                     </div>
                                 </form>
                                 <div class="othre-text">
-                                    <p>Have not any account? <a href="signup.html">Register Now</a></p>
+                                    <p>Have any account? <a href="{{ route('login') }}">Login</a></p>
                                 </div>
                             </div>
                         </div>
